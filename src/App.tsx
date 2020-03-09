713f4de6cdc308aef0219a874a437b10837213a6
aymen hamzaoui <elhamzaouiaymen@gmail.com>
2020-03-09 15:50:56 +0100

    feat: add notification toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Suspense } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import ContentWrapper from './components/layouts/ContentWrapper';
+import Toaster from './components/shared/toaster/Toaster';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Suspense fallback="loading">
+        <ContentWrapper />
+      </Suspense>
     </div>
   );
 }
